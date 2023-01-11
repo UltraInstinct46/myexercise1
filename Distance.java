31464c11f083b52ee9c47400e58b0079351ca2e0
killua <Restuaerestu291@gmail.com>
2023-01-11 14:20:20 +0700

    update

--- Distance.java
+++ Distance.java
@@ -7,13 +7,16 @@ public class Distance{
         int u,a,t,Distance;
         System.out.println("Enter the value of Initial Velocity:");
         Scanner sc1 = new Scanner(System.in);
-        u=sc1.nextInt();
+        // u=sc1.nextInt();
+        u=1;
         System.out.println("Enter the value of acceleration:");
         Scanner sc2 = new Scanner(System.in);
-        a= sc2.nextInt();
+        // a= sc2.nextInt();
+        a=1;
         System.out.println("Enter the value of Time:");
         Scanner sc3 = new Scanner(System.in);
-        t=sc2.nextInt();
+        // t=sc2.nextInt();
+        t=1;
         Distance = u*t+(a*t*t)/2;
         System.out.println("Distance Travelled ="+Distance+" Meter");
     }
